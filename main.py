1b3f5496fc6585b26bdbec9ff2f173789e90d865
jordan95v <jordan.dfrsne@gmail.com>
2022-10-12 19:08:47 +0200

    Refactoring.

--- main.py
+++ main.py
@@ -1,11 +1,34 @@
 import asyncio
+import enum
 from pathlib import Path
+from typing import Any
 import typer
 from core.client import Client
 from core.models.manga import Chapter, Manga
 from core.utils.exceptions import MangaNotFound
 
+
+class Mode(enum.Enum):
+    DL_ALL: enum.auto = enum.auto()
+    DL_CHAPTER: enum.auto = enum.auto()
+
+
 app: typer.Typer = typer.Typer()
+
+
+async def ask_user(mode: Mode) -> Any:
+    name: str = typer.prompt("Enter the mange name", type=str)
+    path: str = typer.prompt(
+        "Enter the path of stored scan", default=Path().home(), type=Path
+    )
+    if mode == Mode.DL_ALL:
+        limit: str = typer.prompt(
+            "Enter le limit of chapter downloaded at the same time", type=int, default=5
+        )
+        return name, path, limit
+    else:
+        chapter_num: str = typer.prompt("Enter the chapter number", type=str)
+        return name, chapter_num, path
 
 
 async def _dl_chapter(
@@ -15,7 +38,11 @@ async def _dl_chapter(
         await client.download_images(path, chapter, limit=5)
 
 
-async def scrap_chapter(name: str, chapter_num: str, path: Path) -> None:
+async def scrap_chapter() -> None:
+    name: str
+    chapter_num: str
+    path: Path
+    name, chapter_num, path = await ask_user(Mode.DL_CHAPTER)
     async with Client() as client:
         try:
             manga: Manga = await client.get_manga_info(name)  # type: ignore
@@ -28,7 +55,11 @@ async def scrap_chapter(name: str, chapter_num: str, path: Path) -> None:
                     break
 
 
-async def scrap(name: str, path: Path, limit: int) -> None:
+async def scrap() -> None:
+    name: str
+    path: Path
+    limit: int
+    name, path, limit = await ask_user(Mode.DL_ALL)
     async with Client() as client:
         try:
             manga: Manga = await client.get_manga_info(name)  # type: ignore
@@ -41,7 +72,7 @@ async def scrap(name: str, path: Path, limit: int) -> None:
             await asyncio.gather(
                 *[
                     _dl_chapter(chapter, manga_dir, client, sem)
-                    for chapter in manga.chapterss  # type: ignore
+                    for chapter in manga.chapters  # type: ignore
                     if (manga_dir / f"{chapter.title}.cbz") not in manga_dir.iterdir()
                 ]
             )
@@ -49,24 +80,12 @@ async def scrap(name: str, path: Path, limit: int) -> None:
 
 @app.command()
 def dl_all():
-    name: str = typer.prompt("Enter the mange name", type=str)
-    path: str = typer.prompt(
-        "Enter the path of stored scan", default=Path().home(), type=Path
-    )
-    limit: str = typer.prompt(
-        "Enter le limit of chapter downloaded at the same time", type=int, default=5
-    )
-    asyncio.run(scrap(name, path, limit))
+    asyncio.run(scrap())
 
 
 @app.command()
 def dl_chapter():
-    name: str = typer.prompt("Enter the mange name", type=str)
-    chapter_num: str = typer.prompt("Enter the chapter number", type=str)
-    path: str = typer.prompt(
-        "Enter the path of stored scan", default=Path().home(), type=Path
-    )
-    asyncio.run(scrap_chapter(name, chapter_num, path))
+    asyncio.run(scrap_chapter())
 
 
 if __name__ == "__main__":
