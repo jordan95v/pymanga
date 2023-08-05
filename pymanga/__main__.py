b3457cdd003b91dd5ad485abaed423f1636c0e99
jdr <jordan.dfrsne@gmail.com>
2023-08-05 13:16:38 +0200

    Moved handle function outside of main function.

--- pymanga/__main__.py
+++ pymanga/__main__.py
@@ -10,6 +10,26 @@ from pymanga.models import Chapter
 app: typer.Typer = typer.Typer()
 
 
+async def handle(
+    manga_name: str, output: Path, from_: int | None, to: int | None
+) -> None:
+    async with Client() as client:
+        chapters: list[Chapter] = await client.get_chapters(manga_name)
+        if from_ is not None and to is not None:
+            chapters = chapters[from_ - 1 : to]
+        elif from_ is not None:
+            chapters = chapters[from_ - 1 :]
+        print(f"Downloading [green]{len(chapters)}[/green] chapters ...\n")
+
+        for chapter in chapters:
+            print(f"[bold]Downloading[/bold] [blue]{chapter.name}[/blue]")
+            await client.download_chapter(chapter, output, 3)
+            print(
+                f"[bold green]Downloaded[/bold green]"
+                f" [blue]{chapter.name}[/blue] to [yellow]{output}[/yellow] !\n"
+            )
+
+
 @app.command()
 def main(
     manga_name: Annotated[str, typer.Argument(help="Name of the manga.")],
@@ -17,35 +37,18 @@ def main(
     from_: Annotated[
         Union[int, None],
         typer.Argument(
-            help="Download from this chapter.", rich_help_panel="Secondary arguments"
+            help="Download from this chapter.",
+            rich_help_panel="Secondary arguments",
         ),
     ] = None,
     to: Annotated[
         Union[int, None],
         typer.Argument(
-            help="End download to this chapter.", rich_help_panel="Secondary arguments"
+            help="End download to this chapter.",
+            rich_help_panel="Secondary arguments",
         ),
     ] = None,
 ) -> None:
-    async def handle(
-        manga_name: str, output: Path, from_: int | None, to: int | None
-    ) -> None:
-        async with Client() as client:
-            chapters: list[Chapter] = await client.get_chapters(manga_name)
-            if from_ is not None and to is not None:
-                chapters = chapters[from_ - 1 : to]
-            elif from_ is not None:
-                chapters = chapters[from_ - 1 :]
-            print(f"Downloading [green]{len(chapters)}[/green] chapters ...\n")
-
-            for chapter in chapters:
-                print(f"[bold]Downloading[/bold] [blue]{chapter.name}[/blue]")
-                await client.download_chapter(chapter, output, 3)
-                print(
-                    f"[bold green]Downloaded[/bold green]"
-                    f" [blue]{chapter.name}[/blue] to [yellow]{output}[/yellow] !\n"
-                )
-
     asyncio.run(handle(manga_name, output, from_, to))
 
 
